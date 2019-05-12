59cc3ef3cd86ec29c341b3038325e4c0fb9f81db
Mohamed Hasan <mnt@plusmnt.com>
2019-05-12 14:34:20 +0300

    add threading

--- find.py
+++ find.py
@@ -1,8 +1,10 @@
+from concurrent.futures import ThreadPoolExecutor as PoolExecutor
+import multiprocessing
 import requests
 import json
 from itertools import product
 from string import ascii_lowercase
-#import progressbar
+
 import pyexcel
 #setting
 api_url = 'https://domain-registry.appspot.com/check?domain='
@@ -16,23 +18,26 @@ gen_domain=map(''.join, product(ascii_lowercase, repeat=domain_length))
 domain_list =list(gen_domain)
 #domain_name = "abc"
 result_list=[]
-#progressbar.streams.wrap_stderr()
+
 #functions
 def is_available(a):
 	if a is True:
 		return "Available"
 	else:
 		return "Not available"
-#def find_domain(domain):
-
-
-for domain in domain_list: #progressbar.progressbar(domain_list, redirect_stdout=True):
+def find_domain(domain):
 	request_url = api_url+domain+domain_ltd
 	response = requests.get(request_url)
 	json_data = json.loads(response.content)
 	json_data['domain'] = domain+domain_ltd
 	print(json_data['domain']  +" is " + is_available(json_data['available'] ))
 	result_list.append(json_data)
+	
+
+with PoolExecutor(max_workers =multiprocessing.cpu_count()) as executor:
+	for _ in executor.map(find_domain,domain_list):
+		pass
+
 
 print("Saving on CSV file...")
 # https://github.com/pyexcel/pyexcel
